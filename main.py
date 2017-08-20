c94b825861427149fbaf1df2d49487dbfa326ec4
Pedro da Luz <pedro.luz@hotmail.com.br>
2017-08-20 01:05:07 -0300

    Fremarate de 30fps adicionado

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ if __name__ == '__main__':
                
     screen = pygame.display.set_mode((640, 480), 0, 32)
     
+    clock = pygame.time.Clock()
+    
     pygame.display.set_caption('Hello World')
     
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
-            screen.blit(pygame.Surface(screen.get_size()), (0,0))
-            pygame.display.update()
+        screen.blit(pygame.Surface(screen.get_size()), (0,0))
+        pygame.display.update()
+        time_passed = clock.tick(30)
